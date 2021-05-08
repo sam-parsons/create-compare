a43284f51937d6519f94cda4a8ffe07b6e49b4cf
Sam Parsons <samparsons269@gmail.com>
2021-05-08 18:48:02 -0400

    refactored module parameters and homogenized logic

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-function createStringCompareFunc(property, propertyArr) {
-  return function stringCompareFunc(a, b) {
-    if (a[property] < b[property]) return -1;
-    if (a[property] > b[property]) return 1;
-    return 0;
-  };
-}
-
-function createNumberCompareFunc(property, propertyArr) {
-  return function numberCompareFunc(a, b) {
+function createCompareFunc(propertyArr) {
+  return function (a, b) {
     const aObj = findLocalObject(a, propertyArr);
     const bObj = findLocalObject(b, propertyArr);
-    return (
-      aObj[propertyArr[propertyArr.length - 1]] -
+    if (
+      aObj[propertyArr[propertyArr.length - 1]] <
       bObj[propertyArr[propertyArr.length - 1]]
-    );
+    )
+      return -1;
+    if (
+      aObj[propertyArr[propertyArr.length - 1]] >
+      bObj[propertyArr[propertyArr.length - 1]]
+    )
+      return 1;
+    return 0;
   };
 }
 
@@ -27,34 +26,10 @@ function findLocalObject(object, propertyArr) {
   return findLocalObject(object[nextProperty], newPropertyArr);
 }
 
-function findPropertyDataType(object, propertyArr) {
-  const newPropertyArr = Object.assign([], propertyArr);
-  if (newPropertyArr.length === 1 && object.hasOwnProperty(newPropertyArr[0])) {
-    return typeof object[newPropertyArr[0]];
-  }
-  if (newPropertyArr.length === 0) return;
-  const nextProperty = newPropertyArr.shift();
-  return findPropertyDataType(object[nextProperty], newPropertyArr);
-}
-
-module.exports = function (array, property, opts) {
-  let compareFunc;
-
-  // split the property on "."
+module.exports = function (property) {
+  // split the properties on "."
+  // turns "info.age" -> ["info", "age"]
   const propertyArray = property.split('.');
 
-  const propertyType = findPropertyDataType(array[0], propertyArray);
-
-  switch (propertyType) {
-    case 'string':
-      compareFunc = createStringCompareFunc(property, propertyArray);
-      break;
-    case 'number':
-      compareFunc = createNumberCompareFunc(property, propertyArray);
-      break;
-  }
-
-  array.sort(compareFunc);
-
-  return array;
+  return createCompareFunc(propertyArray);
 };
