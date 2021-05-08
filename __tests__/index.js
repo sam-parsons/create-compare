0392bcd2507d0f6e99ae40531dd0f214434799cf
Sam Parsons <samparsons269@gmail.com>
2021-05-08 18:47:30 -0400

    refactored to new module return value of function

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,14 +1,23 @@
-const simpleSorter = require('../index');
+const createCompare = require('../index');
 
-test('should return array that was passed in', () => {
+test('should return a function', () => {
   const initialArray = [{ name: 'sam' }, { name: 'floyd' }];
-  const sortedArray = simpleSorter(initialArray, 'name');
-  expect(sortedArray).toBe(initialArray);
+  const compareFn = createCompare('name');
+  expect(typeof compareFn).toBe('function');
+});
+
+xtest('should sort array of string primitives', () => {
+  const initialArray = ['sam', 'floyd', 'robert'];
+});
+
+xtest('should sort array of number primitives', () => {
+  const initialArray = [23, 1, 7];
 });
 
 test('should sort by top level attribute with strings', () => {
   const initialArray = [{ name: 'sam' }, { name: 'floyd' }, { name: 'robert' }];
-  const sortedArray = simpleSorter(initialArray, 'name');
+  const compareFn = createCompare('name');
+  const sortedArray = [...initialArray.sort(compareFn)];
   expect(sortedArray[0].name).toBe('floyd');
   expect(sortedArray[1].name).toBe('robert');
   expect(sortedArray[2].name).toBe('sam');
@@ -20,22 +29,11 @@ test('should sort by top level attribute with numbers', () => {
     { name: 'floyd', age: 17 },
     { name: 'robert', age: 63 },
   ];
-  const sortedArray = simpleSorter(initialArray, 'age');
+  const compareFn = createCompare('age');
+  const sortedArray = [...initialArray.sort(compareFn)];
   expect(sortedArray[0].name).toBe('floyd');
   expect(sortedArray[1].name).toBe('sam');
   expect(sortedArray[2].name).toBe('robert');
-});
-
-xtest('should sort descending with numbers', () => {
-  const initialArray = [
-    { name: 'sam', age: 32 },
-    { name: 'floyd', age: 17 },
-    { name: 'robert', age: 63 },
-  ];
-  const sortedArray = simpleSorter(initialArray, 'age', { descending: true });
-  expect(sortedArray[0].name).toBe('robert');
-  expect(sortedArray[1].name).toBe('sam');
-  expect(sortedArray[2].name).toBe('floyd');
 });
 
 test('should sort nested objects', () => {
@@ -44,10 +42,21 @@ test('should sort nested objects', () => {
     { info: { name: 'floyd', age: 17 } },
     { info: { name: 'robert', age: 63 } },
   ];
-  const sortedArray = simpleSorter(initialArray, 'info.age', {
-    descending: true,
-  });
+  const compareFn = createCompare('info.age');
+  const sortedArray = [...initialArray.sort(compareFn)];
   expect(sortedArray[0].info.name).toBe('floyd');
   expect(sortedArray[1].info.name).toBe('sam');
   expect(sortedArray[2].info.name).toBe('robert');
+});
+
+xtest('should sort descending with numbers', () => {
+  const initialArray = [
+    { name: 'sam', age: 32 },
+    { name: 'floyd', age: 17 },
+    { name: 'robert', age: 63 },
+  ];
+  const sortedArray = createCompare('age', { descending: true });
+  expect(sortedArray[0].name).toBe('robert');
+  expect(sortedArray[1].name).toBe('sam');
+  expect(sortedArray[2].name).toBe('floyd');
 });
