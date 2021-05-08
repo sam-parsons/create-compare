3aae1e33f3ea031fd6af01247d65e71ab8690873
Sam Parsons <samparsons269@gmail.com>
2021-05-08 14:38:41 -0400

    sorts string and number property types

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-module.exports = function (array, property, opts) {
-  return array.sort(function (a, b) {
+function createStringCompareFunc(property) {
+  return function stringCompareFunc(a, b) {
+    if (a[property] < b[property]) return -1;
+    if (a[property] > b[property]) return 1;
+    return 0;
+  };
+}
+
+function createNumberCompareFunc(property) {
+  return function numberCompareFunc(a, b) {
     return a[property] - b[property];
-  });
+  };
+}
+
+module.exports = function (array, property, opts) {
+  let compareFunc;
+
+  switch (typeof array[0][property]) {
+    case 'string':
+      compareFunc = createStringCompareFunc(property);
+      break;
+    case 'number':
+      compareFunc = createNumberCompareFunc(property);
+      break;
+  }
+
+  return array.sort(compareFunc);
 };
